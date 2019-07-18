0dc4aa51b42999bc3d48d0b3f9c7b0f2fe75f20a
prin-r <lang_zane@hotmail.com>
2019-07-18 21:29:24 +0700

    reduce erc20 interface

--- contract/SimpleLending.sol
+++ contract/SimpleLending.sol
@@ -1,11 +1,7 @@
 pragma solidity 0.5.10;
 
 interface ERC20Interface {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
     function transfer(address to, uint tokens) external returns (bool success);
-    function approve(address spender, uint tokens) external returns (bool success);
     function transferFrom(address from, address to, uint tokens) external returns (bool success);
 }
 
